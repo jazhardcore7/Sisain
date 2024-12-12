4bc4ff4cef4011f77e088626a3f7077c1b3033a6
sh44cch1 <aishacink@gmail.com>
2024-12-12 23:27:02 +0700

    onboarding 3 halaman sama splash screen

--- app/src/main/java/com/sisain/capstone/ui/ui/views/boarding/OnBoardingActivity.kt
+++ app/src/main/java/com/sisain/capstone/ui/ui/views/boarding/OnBoardingActivity.kt
@@ -2,46 +2,38 @@ package com.sisain.capstone.ui.ui.views.boarding
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.sisain.capstone.MainActivity
 import com.sisain.capstone.R
 import com.sisain.capstone.ui.ui.views.auth.LoginActivity
 import com.sisain.capstone.ui.ui.views.auth.RegisterActivity
-
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 class OnBoardingActivity : AppCompatActivity() {
 
-     lateinit var viewPager: ViewPager2
-    private lateinit var onboardingAdapter: OnboardingAdapter
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Set konten OnBoardingActivity
         setContentView(R.layout.activity_onboarding)
 
-        // Setup ViewPager2 dan adapter-nya
-        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
-        val adapter = OnboardingAdapter(this)
+        viewPager = findViewById(R.id.viewPager)
+        val dotsIndicator: DotsIndicator = findViewById(R.id.dotsIndicator)
+
+        val adapter = OnboardingFragmentAdapter(this)
         viewPager.adapter = adapter
+        dotsIndicator.attachTo(viewPager)
+    }
 
-        // Menambahkan Listener pada halaman terakhir
-        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                if (position == 2) { // Halaman ketiga (index 2)
-                    val sharedPreferences = getSharedPreferences("onboarding_pref", MODE_PRIVATE)
-                    val editor = sharedPreferences.edit()
-                    editor.putBoolean("isOnboardingCompleted", true)
-                    editor.apply()
+    fun navigateToPage(pageIndex: Int) {
+        viewPager.currentItem = pageIndex
+    }
 
-                    // Bisa arahkan ke MainActivity setelah onboarding selesai
-                    startActivity(Intent(this@OnBoardingActivity, MainActivity::class.java))
-                    finish() // Menutup OnBoardingActivity
-                }
-            }
-        })
+    fun finishOnboarding() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
         val createAccountButton = findViewById<Button>(R.id.btn_create_account)
         createAccountButton.setOnClickListener {
         startActivity(Intent(this, RegisterActivity::class.java))
@@ -52,4 +44,3 @@ class OnBoardingActivity : AppCompatActivity() {
         }
     }
 }
-
